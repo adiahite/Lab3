980f547eb1e2a698df33c4885daef33111257155
Adia Hite <adiahite1@gmail.com>
2025-09-04 08:26:30 -0700

    lab 3.1A is completed!

---
+++ lab3_1.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file lab3_1.cpp
+ *
+ * This program finds the average of two numbers.
+ * It contains two errors that must be fixed.
+ *
+ * @author Adia Hite
+ * @date   09.04.25
+ */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int size = 0;    // The number of values to be averaged
+    double num1 = 0.0,     // User input number 1
+           num2 = 0.0,     // User input number 2
+           average = 0.0;  // Average of num1 and num2
+
+    // Get the two numbers
+    cout << "Enter two numbers separated by one or more spaces: ";
+    cin  >> num1; 
+    cin  >> num2;
+
+    // Calculate the average
+    average = num1 + num2 / size;
+
+    // Display the average
+    cout << "The average of the two numbers is: " << average << endl;
+
+    return 0;
+}
